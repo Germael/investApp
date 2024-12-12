551fdeb7466341c3a2f4781a91def4318ce27479
Mykola <mykola.shevchenko.ext@fashion-digital.de>
2024-12-12 14:20:20 +0200

    extend ticker info

--- src/route/parsing_route.py
+++ src/route/parsing_route.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 
 from database import get_session
 from database.models import TickerInfo
-from database.query import add_tickers, get_all_tickers, add_tickers_info
+from database.query import add_tickers, get_all_tickers, add_tickers_info, delete_tickers_info
 from repository.bot import Bot
 from repository.stock_scrapper import StockScrapper
 
@@ -32,24 +32,42 @@ def parse_sp500_tickers(
     ticker_info_list = []
     tickers = get_all_tickers(db_session)
 
-    for ticker in tickers:
-        ticker_data = scraper.get_ticker_info(ticker.name)
-        if ticker_data:
-            ticker_info_list.append(
-                TickerInfo(
+    try:
+        delete_tickers_info(db_session)
+
+        for ticker in tickers:
+            ticker_data = scraper.get_ticker_info(ticker.name)
+            if ticker_data:
+                ticker_info = TickerInfo(
                     ticker_name=ticker.name,
+                    short_name=ticker_data.get("shortName"),
+                    sector=ticker_data.get("sector"),
                     open=ticker_data.get("open"),
+                    current_price=ticker_data.get("currentPrice"),
                     previous_close=ticker_data.get("previousClose"),
                     day_low=ticker_data.get("dayLow"),
                     day_high=ticker_data.get("dayHigh"),
                     dividend_rate=ticker_data.get("dividendRate"),
                     forward_pe=ticker_data.get("forwardPE"),
+                    forward_eps=ticker_data.get("forwardEps"),
                     volume=ticker_data.get("volume"),
                     market_cap=ticker_data.get("marketCap"),
+                    target_high_price=ticker_data.get("targetHighPrice"),
+                    target_low_price=ticker_data.get("targetLowPrice"),
+                    target_median_price=ticker_data.get("targetMedianPrice"),
+                    recommendation_key=ticker_data.get("recommendationKey"),
                 )
-            )
+                if ticker_info.previous_close and ticker_info.current_price:
+                    ticker_info.current_price_change = ((
+                                                                ticker_info.current_price - ticker_info.previous_close) / ticker_info.previous_close) * 100
+                ticker_info_list.append(ticker_info)
 
-    add_tickers_info(db_session, ticker_info_list)
+            if len(ticker_info_list) >= 50:
+                add_tickers_info(db_session, ticker_info_list)
+                ticker_info_list.clear()
 
+        add_tickers_info(db_session, ticker_info_list)
 
-
+        db_session.commit()
+    except Exception as e:
+        db_session.rollback()
