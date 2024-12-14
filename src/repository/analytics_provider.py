9ae611924c18242f898639cea6f93951b2fb1304
Mykola <mykola.shevchenko.ext@fashion-digital.de>
2024-12-14 12:29:04 +0200

    adapt get_ticker_info

--- src/repository/analytics_provider.py
+++ src/repository/analytics_provider.py
@@ -1,9 +1,10 @@
 from database import get_session
+from repository.stock_scrapper import StockScrapper
 
 
 class AnalyticsProvider:
     def __init__(self):
         self.db_session = get_session()
 
-    def get_analytics_message(self, ticker: str) -> str:
+    def get_analytics_message(self, scraper: StockScrapper, ticker: str) -> str:
         pass
